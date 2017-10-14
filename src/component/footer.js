37397eed07c25845c24ea54b71ec86cbd1a2f704
habeeb <habibmail31@yahoo.com>
2017-10-14 22:09:12 +0100

    footer updated

--- src/component/footer.js
+++ src/component/footer.js
@@ -4,8 +4,8 @@ class Footer extends Component {
     render() {
         if(window.location.pathname == "/cart") return null;
         return (
-
-            <footer className="container" style={{marginTop:"20px"}}>
+            <footer>
+            <div className="container" style={{marginTop:"20px"}}>
                 <div className="news-letter" >
 
                     <div className="row">
@@ -60,6 +60,90 @@ class Footer extends Component {
                         </li>
                     </ul>
                 </div>
+
+      
+                        
+                    
+            </div>
+            <div className="footer" style={{marginTop:"30px"}}>
+                <div className="container">
+                    <div className="row firstrow">
+                        <div className="col-sm-3 zero">
+                            <h4>About Us</h4>
+                            <div className="grid1">
+                                <p>
+                                  sit a asperiores maxime est adipisci. Laudantium suscipit quia nobis.lorem
+
+                                </p>
+                                <p><i className="fa fa-phone"></i> +234-8181362484</p>
+                                <p><a href=""><i className="fa fa-envelope"></i> habibmail31@gmail.com </a></p>
+                                <p className="icons"> <a href="" className="btn btn-xs"><i className="fa fa-git"></i></a>
+                                    <a href="" className="btn btn-xs"><i className="fa fa-facebook"></i> </a>
+                                    <a href="" className="btn btn-xs"><i className="fa fa-instagram"></i></a>
+                                    <a href="" className="btn btn-xs"> <i className="fa fa-twitter"></i> </a>
+                                    <a href="" className="btn btn-xs"><i className="fa fa-yahoo"></i> </a>
+                                    <a href="" className="btn btn-xs"><i className="fa fa-linkedin"></i></a> </p>
+
+                            </div>
+                        </div>
+                        <div className="col-sm-3 zero ">
+                            <div className="grid2">
+                                <h4>Information</h4>
+                                <ul>
+                                    <li><a href="#">  About Us </a></li>
+                                    <li><a href="#">My Account </a></li>
+                                    <li><a href="#"> Shipping & Returns  </a></li>
+                                    <li><a href="#"> Order History  </a></li>
+                                    <li><a href="#">  Help & F.A.Q  </a></li>
+                                    <li><a href="#">  Conditions  </a></li>
+                                    <li><a href="#"> Contact Us  </a></li>
+                                    <li><a href="#">  Manufacturers  </a></li>
+                                    <li><a href="#">  Online Supports  </a></li>
+                                    <li><a href="#">  privacy </a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-sm-3 zero">
+                            <div className="grid3">
+                                <h4>Product Tags</h4>
+                                <button type="button" className="btn " name="button">phones</button>
+                                <button type="button" className="btn" name="button">tv</button>
+                                <button type="button" className="btn" name="button">tech</button>
+                                <button type="button" className="btn" name="button">cloths</button>
+                                <button type="button" className="btn" name="button">kitchen</button>
+                                <button type="button" className="btn " name="button">phones</button>
+                                <button type="button" className="btn" name="button">tv</button>
+                                <button type="button" className="btn" name="button">tech</button>
+                                <button type="button" className="btn" name="button">cloths</button>
+                                <button type="button" className="btn" name="button">kitchen</button>
+                                <button type="button" className="btn " name="button">phones</button>
+                                <button type="button" className="btn" name="button">tv</button>
+                                <button type="button" className="btn" name="button">tech</button>
+                                <button type="button" className="btn" name="button">cloths</button>
+                                <button type="button" className="btn" name="button">kitchen</button>
+                            </div>
+                        </div>
+                        <div className="col-sm-3 zero">
+                            <div className="grid4">
+                                <h4>Recent Post</h4>
+                                <p> Lorem ipsum dolor sit amet consectetur adipiscing elitos.X
+                            <br />by <a href="#">Habeeb</a> </p>
+                                <p> Lorem ipsum dolor sit amet consectetur adipiscing elitos.X
+                            <br />by <a href="#">Habeeb</a> </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="copyright">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm-12 zero">
+                            <span>©2017 KASTORE- inc. All rights Reserved</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
             </footer>
         );
     }
